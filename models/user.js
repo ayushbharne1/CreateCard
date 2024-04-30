8a63808192810238eaad73a2ae49506b2b5ad42d
ayush <ayushbharne@gmail.com>
2024-04-30 11:25:51 +0530

    update app.js

--- models/user.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect("mongodb://127.0.0.1:27017/UserPage");
-
-const userSchema = mongoose.Schema({
-    image: String,
-    name: String,
-    email: String
-})
-module.exports = mongoose.model("user", userSchema);
